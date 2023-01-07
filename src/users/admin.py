4d78071f76992fe24b2803e260528f8b47840422
Julia-Shejko <Julia_Shejko@i.ua>
2023-01-07 16:18:20 +0200

    Working with the user model and the administrative panel.
    The project documentation has been updated.
    
    Added ".env.default" file.
    
    User model creation in progress:
    - the user model has been updated;
    - added user roles.
    
    Started working with the administrative panel:
    - created a superuser;
    - update the display of the administrator panel:
        * it is possible to sort users by roles.

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin  # noqa F401, F403
+from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ["user_permissions", "groups"]
+    readonly_fields = [
+        "password",
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "email",
+    ]
+    list_display = ["email", "first_name", "last_name", "role", "is_active"]
+    list_filter = ["role"]
+    search_fields = ["email", "last_name"]
