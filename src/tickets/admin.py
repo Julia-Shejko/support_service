06441a0320e4c85d36c64dbcdd39537eb97dae50
Julia-Shejko <Julia_Shejko@i.ua>
2023-01-22 20:37:40 +0200

    Work with users and tickets.
    User model creation in progress:
    - The user registration endpoint is updated
    - User can be created by using the HTTP POST /users
    - The rest_framework.CreateAPIView is used as a request handler
    - The save() to the database logic is handled by the UserSerializer
    - The password field is excluded from the user create response
    
    Started working with tickets:
    - The ability to output a list of tickets has been implemented by using the HTTP GET /tickets/list/
    - The possibility of receiving complete information on a specific ticket has been implemented by using the HTTP GET /tickets/<id>
    - - Started work on the possibility of creating a ticket by a registered user

---
+++ src/tickets/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from shared.django import TimeStampReadonlyAdmin
+from tickets.models import Ticket
+
+
+@admin.register(Ticket)
+class TicketsAdmin(TimeStampReadonlyAdmin):
+    pass
